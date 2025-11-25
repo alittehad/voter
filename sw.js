b97f1fa212eb5e68724c049de0e82023fe82b60c
alittehad <ngogrant454@gmail.com>
2025-11-25 03:54:41 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
-const CACHE_NAME = "voter-app-cache";
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("voter-app-v1").then((cache) => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ]);
+    })
+  );
+});
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
