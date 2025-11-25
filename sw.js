e4cb2b22e8d6235d3754d0ef7331db56f7816961
alittehad <ngogrant454@gmail.com>
2025-11-25 03:01:24 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,9 @@
-const CACHE_NAME = "voter-app-cache-v1";
-const urlsToCache = [
-  "/index.html",
-  "/login-page.html",
-  "/manifest.json",
-  "/sw.js",
-  "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js",
-  "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"
-];
-
-// Install event
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+  self.skipWaiting();
 });
 
-// Fetch event
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+  return self.clients.claim();
 });
