caf828d6f3680edca2328311e79c0805080578f1
alittehad <ngogrant454@gmail.com>
2025-11-25 03:21:59 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,7 @@
-const CACHE_NAME = "voter-app-v1";
-const URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
-
-// Install Event
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-// Fetch Event
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Update Cache Version Handling
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
-});
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("/sw.js")
+      .then(reg => console.log("ServiceWorker registered:", reg))
+      .catch(err => console.log("SW registration failed:", err));
+  });
+}
