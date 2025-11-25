24463b4792e6ffce8abb6b43f1d3164f7f9b9521
alittehad <ngogrant454@gmail.com>
2025-11-25 03:08:26 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,40 @@
-<script>
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then(res => console.log("SW registered", res))
-  .catch(err => console.log("SW error", err));
-}
-</script>
+const CACHE_NAME = "voter-app-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install Event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Fetch Event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update Cache Version Handling
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
